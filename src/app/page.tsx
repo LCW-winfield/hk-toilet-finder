72412fa5ecfd42639899dbde33a14f2ef9dabd85
HK Toilet Finder <admin@hk-toilet-finder.app>
2026-05-27 15:50:42 +0800

    Set home page to force-dynamic to skip DB query during build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { HomeClient } from '@/components/home-client';
 import { getToilets } from '@/lib/toilets';
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const toilets = await getToilets();
 
